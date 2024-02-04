1f73b5fd74f9bfa179430f309ccd72a4d225c29f
Vaibhav <vaibhavmonu8309@gmail.com>
2024-02-04 23:24:01 +0530

    use generic ui-state from network module

--- authorization/build.gradle.kts
+++ authorization/build.gradle.kts
@@ -80,4 +80,7 @@ dependencies {
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Core
+    implementation(project(":core:network"))
 }
