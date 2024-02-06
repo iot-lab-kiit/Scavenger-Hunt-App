82bde64ddb7599709ab81cc3ee6b507a701dcc57
Vaibhav <vaibhavmonu8309@gmail.com>
2024-02-06 14:52:50 +0530

    ui: update team screen

--- dashboard/src/main/java/in/iot/lab/dashboard/ui/screen/team/TeamScreen.kt
+++ dashboard/src/main/java/in/iot/lab/dashboard/ui/screen/team/TeamScreen.kt
@@ -5,34 +5,61 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import `in`.iot.lab.dashboard.R
 import `in`.iot.lab.design.components.PrimaryButton
 import `in`.iot.lab.design.theme.ScavengerHuntTheme
 import `in`.iot.lab.design.theme.background
 import `in`.iot.lab.network.data.models.team.RemoteTeam
+import `in`.iot.lab.network.state.UiState
 
 @Composable
-internal fun TeamRoute() {
-    TeamScreen()
+internal fun TeamRoute(
+    viewModel: TeamScreenViewModel = hiltViewModel(),
+    onTeamScoreClick: () -> Unit = {}
+) {
+    val teamState by viewModel.teamData.collectAsState()
+
+    when (teamState) {
+        is UiState.Success -> {
+            val data = (teamState as UiState.Success<RemoteTeam>).data
+            TeamScreen(
+                team = data,
+                onTeamScoreClick = onTeamScoreClick,
+                mainQuests = data.route?.hints?.size ?: 0,
+                // TODO: Add sideQuests here, currently its not implemented in the RemoteTeam model
+                sideQuests = 10,
+                mainQuestProgress = data.numMain ?: 0,
+                sideQuestProgress = data.numSide ?: 0
+            )
+        }
+        is UiState.Loading -> {
+            CircularProgressIndicator()
+        }
+        else -> {}
+    }
 }
 
 @Composable
 internal fun TeamScreen(
-    mainQuests: List<Boolean> = remember { (1..10).map { Math.random() < .5 }.toList() },
-    sideQuests: List<Boolean> = remember { (1..10).map { Math.random() < .5 }.toList() },
+    mainQuests: Int = 10,
+    sideQuests: Int = 10,
+    mainQuestProgress: Int = 1,
+    sideQuestProgress: Int = 3,
     team: RemoteTeam = RemoteTeam(),
     onTeamScoreClick: () -> Unit = {}
 ) {
-    // GET This data from the API
-
     Scaffold(
         containerColor = background,
         contentColor = MaterialTheme.colorScheme.onPrimary,
@@ -51,12 +78,17 @@ internal fun TeamScreen(
             }
         }
     ) {
+        // Temp value so the compiler don't complain
+        val tempMainQuests: List<Boolean> = remember { (1..10).map { Math.random() < .5 }.toList() }
+        val tempSideQuests: List<Boolean> = remember { (1..10).map { Math.random() < .5 }.toList() }
         TeamScreenContent(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it),
-            mainQuests = mainQuests,
-            sideQuests = sideQuests,
+            mainQuests = tempMainQuests,
+            sideQuests = tempSideQuests,
+//            mainQuestProgress = mainQuestProgress,
+//            sideQuestProgress = sideQuestProgress,
             team = team,
             onTeamScoreClick = onTeamScoreClick
         )
