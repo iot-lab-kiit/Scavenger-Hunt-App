d8299dcf63c7191be1332cd6dc95fbda6d0f0181
Vaibhav <vaibhavmonu8309@gmail.com>
2024-02-06 14:52:07 +0530

    setup data,di,viewmodel for TeamScreen

--- dashboard/build.gradle.kts
+++ dashboard/build.gradle.kts
@@ -73,4 +73,8 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-compiler:2.48")
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
---
+++ dashboard/src/main/java/in/iot/lab/dashboard/data/repository/DashboardRepository.kt
@@ -0,0 +1,8 @@
+package `in`.iot.lab.dashboard.data.repository
+
+import `in`.iot.lab.network.data.models.team.RemoteTeam
+import `in`.iot.lab.network.state.ResponseState
+
+interface DashboardRepository {
+    suspend fun getTeamById(id: String): ResponseState<RemoteTeam>
+}
