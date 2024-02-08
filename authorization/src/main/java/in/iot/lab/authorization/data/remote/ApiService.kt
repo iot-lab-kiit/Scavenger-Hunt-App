3f5e7c4e9a54f186e2678a27c7bc54db13b69c32
Vaibhav <vaibhavmonu8309@gmail.com>
2024-02-09 00:29:43 +0530

    update auth flow

--- authorization/src/main/java/in/iot/lab/authorization/data/remote/ApiService.kt
+++ authorization/src/main/java/in/iot/lab/authorization/data/remote/ApiService.kt
@@ -2,11 +2,12 @@ package `in`.iot.lab.authorization.data.remote
 
 import `in`.iot.lab.authorization.domain.model.AuthRequest
 import `in`.iot.lab.authorization.domain.model.AuthResponse
+import `in`.iot.lab.network.data.models.Response
 import `in`.iot.lab.network.utils.NetworkConstants
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST(NetworkConstants.AUTH_ENDPOINT)
-    suspend fun postAuthentication(@Body request: AuthRequest): AuthResponse
+    suspend fun postAuthentication(@Body request: AuthRequest): Response<AuthResponse>
 }
